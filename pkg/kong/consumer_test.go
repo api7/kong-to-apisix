ab97470b8f59a08d3041308081b89237cc47ceb2
帅进超 <shuaijinchao@gmail.com>
2021-09-16 09:35:07 +0800

    fix: migrate consumer panic (#22)
    * fix: migrate consumer panic
    
    * test: add unit test case
    
    * test: update unit test jobs name
    
    * test: update unit test format
    
    * test: update Makefile test path
    
    * chore: update Makefile unit test params

---
+++ pkg/kong/consumer_test.go
@@ -0,0 +1,37 @@
+package kong
+
+import (
+	"testing"
+
+	"github.com/api7/kong-to-apisix/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMigrateConsumer(t *testing.T) {
+	configYaml, kongConsumers := &[]utils.YamlItem{}, &KongConfig{
+		Consumers: Consumers{
+			{
+				CustomId: "test-id-01",
+				Username: "test-user-01",
+				KeyAuthCredentials: []struct {
+					Key string `yaml:"key"`
+				}{
+					{
+						Key: "test-key-01",
+					},
+				},
+			},
+			{
+				CustomId:           "test-id-02",
+				Username:           "test-user-02",
+				KeyAuthCredentials: nil,
+			},
+		},
+	}
+
+	apisixConsumers, err := MigrateConsumer(kongConsumers, configYaml)
+	assert.NoError(t, err)
+	assert.Equal(t, len(kongConsumers.Consumers), len(*apisixConsumers))
+	assert.Equal(t, (*apisixConsumers)[0].Username, "test-user-01")
+	assert.Equal(t, (*apisixConsumers)[1].Username, "test-user-02")
+}
