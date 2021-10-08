662b885b76061023ba3e049985f6a97e7ee7bf09
Janko <shuaijinchao@gmail.com>
2021-10-08 19:49:12 +0800

    feat: add service migration handler

---
+++ pkg/kong/service.go
@@ -0,0 +1,79 @@
+package kong
+
+import (
+	"errors"
+	"fmt"
+	"github.com/api7/kong-to-apisix/pkg/apisix"
+	uuid "github.com/satori/go.uuid"
+)
+
+func MigrateService(kongConfig *Config, apisixConfig *apisix.Config) error {
+	kongServices := kongConfig.Services
+	apisixServices := apisixConfig.Services
+
+	for _, kongService := range kongServices {
+		var apisixService apisix.Service
+		apisixService.ID = kongService.ID
+		apisixService.Name = kongService.Name
+		upstreamID := GenerateApisixServiceUpstream(kongService, apisixConfig)
+		apisixService.UpstreamID = upstreamID
+		apisixServices = append(apisixServices, apisixService)
+	}
+
+	apisixConfig.Services = apisixServices
+	return nil
+}
+
+func GenerateApisixServiceUpstream(kongService Service, apisixConfig *apisix.Config) string {
+	var apisixUpstream apisix.Upstream
+	// apisix upstream id
+	apisixUpstream.ID = uuid.NewV4().String()
+	// apisix upstream nodes
+	var apisixUpstreamNode apisix.UpstreamNode
+	apisixUpstreamNode.Weight = 100
+	apisixUpstreamNode.Host = kongService.Host
+	if kongService.Port == 0 {
+		apisixUpstreamNode.Port = 80
+	} else {
+		apisixUpstreamNode.Port = kongService.Port
+	}
+	apisixUpstream.Nodes = append(apisixUpstream.Nodes, apisixUpstreamNode)
+	// apisix upstream timeout
+	var apisixUpstreamTimeout apisix.UpstreamTimeout
+	apisixUpstreamTimeout.Send = kongService.WriteTimeout
+	apisixUpstreamTimeout.Read = kongService.ReadTimeout
+	apisixUpstreamTimeout.Connect = kongService.ConnectTimeout
+	apisixUpstream.Timeout = apisixUpstreamTimeout
+	// apisix upstream scheme
+	apisixUpstream.Scheme = kongService.Protocol
+	// apisix upstream retries
+	apisixUpstream.Retries = kongService.Retries
+
+	apisixConfig.Upstreams = append(apisixConfig.Upstreams, apisixUpstream)
+
+	return apisixUpstream.ID
+}
+
+func GetKongServiceByID(kongServices *Services, serviceID string) (*Service, error) {
+	if kongServices == nil {
+		return nil, errors.New("kong services is nil or invalid")
+	}
+
+	if len(serviceID) <= 0 {
+		return nil, errors.New("kong service id invalid")
+	}
+
+	var kongService *Service
+	for _, service := range *kongServices {
+		if service.ID == serviceID {
+			kongService = &service
+			break
+		}
+	}
+
+	if kongService != nil {
+		return kongService, nil
+	}
+
+	return nil, fmt.Errorf("kong service id /%s/ not found", serviceID)
+}
